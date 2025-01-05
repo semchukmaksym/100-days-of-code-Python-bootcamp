1aa6c6d72e59dd77360201084e0d9dc6d7732046
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-01-05 23:13:45 +0100

    Added password generator - easy difficulty

---
+++ Day 5/fizz_buzz.py
@@ -0,0 +1,11 @@
+total_range = range(1,101)
+
+for num in total_range:
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
---
+++ Day 5/task.py
@@ -0,0 +1,15 @@
+# student_scores = [123,32,52,123,5123,123,431,231,12,123,451]
+#
+# maximum = 0
+#
+# for score in student_scores:
+#     if score > maximum:
+#        maximum = score
+#
+# print(maximum)
+
+sum_of_100 = 0
+for number in range(1,101):
+    sum_of_100 += number
+
+print(sum_of_100)
