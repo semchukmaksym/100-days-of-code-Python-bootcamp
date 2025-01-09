ab594767237eb9a9129054c5016682d894567554
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-01-09 11:59:31 +0100

    Working on caesar cipher

---
+++ Day 8/caesar_cipher.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type encode to encrypt, type 'decode' to decrypt:\n")
+text = input("Type you message:\n")
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_amount):
+   lower_orig = original_text.lower()
+   encrypted = ""
+   for letter in lower_orig:
+      encrypted_index = alphabet.index(letter) + shift_amount
+      encrypted += alphabet[encrypted_index]
+   print(f"Here is encoded result: {encrypted}")
+
+def decrypt(original_text, shift_amount):
+   lower_orig = original_text.lower()
+   decrypted = ""
+   for letter in lower_orig:
+      encrypted_index = alphabet.index(letter) - shift_amount
+      decrypted += alphabet[encrypted_index]
+   print(f"Here is encoded result: {decrypted}")
+
+if direction == encrypt:
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
