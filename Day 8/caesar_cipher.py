c2bcb08c6d12cb640d34f3d255d53c0bb439bba5
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-01-10 10:23:56 +0100

    Day 8 - completed

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -1,8 +1,8 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import art
 
-direction = input("Type encode to encrypt, type 'decode' to decrypt:\n")
-text = input("Type you message:\n")
-shift = int(input("Type the shift number:\n"))
+print(art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 def caesar(original_text, shift_amount, encode_or_decode):
@@ -13,8 +13,23 @@ def caesar(original_text, shift_amount, encode_or_decode):
 
     encrypted = ""
     for letter in lower_orig:
-        encrypted_index = alphabet.index(letter) + shift_amount
-        encrypted += alphabet[encrypted_index]
-    print(f"Here is {encode_or_decode}d result: {encrypted}")
+        if letter not in alphabet:
+            encrypted += letter
+        else:
+            encrypted_index = alphabet.index(letter) + shift_amount
+            encrypted += alphabet[encrypted_index]
+    print(f"Here is {encode_or_decode}ed result: {encrypted}")
 
-caesar(text, shift, direction)
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type you message:\n")
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'")
+    if restart == 'no':
+        should_continue = False
+        print("Good bye")
