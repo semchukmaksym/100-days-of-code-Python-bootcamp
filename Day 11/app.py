9dfeaee1fadfb775480ba46d5b6af8cfeb2deb90
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-01-13 12:30:35 +0100

    Working on Blackjack game

--- Day 11/app.py
+++ Day 11/app.py
@@ -10,11 +10,44 @@ def deal_card():
     card = random.choice(cards)
     return card
 
+
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from cards"""
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
 user_cards = []
 computer_cards = []
+user_score = -1
+computer_score = -1
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-print(user_cards)
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards},currenct score={user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score >21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass:")
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score !=0 and computer_score <17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
