3cb8430c6d90e434278ace2c5202ae7b873ef48b
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-01-29 11:25:31 +0100

    Finished day 15

--- Day 15/main.py
+++ Day 15/main.py
@@ -40,6 +40,7 @@ def is_resource_sufficient(order_ingredients):
             return False
     return True
 
+
 def process_coins():
     """Returns the total calculated from coins inserted"""
     print("Please insert coins.")
@@ -48,6 +49,26 @@ def process_coins():
     total += int(input("How many nickles?: ")) * 0.05
     total += int(input("How many pennies?: ")) * 0.01
     return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted of False if money is insufficient"""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is {change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}")
+
 
 is_on = True
 
@@ -64,3 +85,5 @@ while is_on:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+               make_coffee(choice, drink["ingredients"])
