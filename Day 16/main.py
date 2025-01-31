67a26993e4ac31843ec1b4c92c28e69d533df447
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-01-31 12:07:31 +0100

    Added day 16

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,35 +1,25 @@
-# from turtle import Turtle, Screen
-#
-# timmy = Turtle()
-#
-# print(timmy)
-#
-# timmy.shape("turtle")
-# timmy.color("coral", "red")
-# timmy.forward(100)
-# timmy.left(45)
-# timmy.forward(100)
-#
-# my_screen = Screen()
-#
-# print(my_screen.canvheight)
-#
-# my_screen.exitonclick()
-#
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from prettytable import PrettyTable
+is_on = True
 
-table = PrettyTable()
-# table.field_names = ["Pokemon name", "Type"]
-# table.add_row(["Pikachu", "Electric"])
-# table.add_row(["Squirtle", "Water"])
-# table.add_row(["Charmander", "Fire"])
-
-table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["ELectric", "Water", "Fire"])
-
-table.align = "l"
-
-print(table)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
 
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
