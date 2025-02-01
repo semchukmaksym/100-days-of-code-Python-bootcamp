e41fe7287e3e9737b93c07b4db9620cf4a4387eb
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-02-01 13:28:23 +0100

    Added day 17

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,21 +1,20 @@
-class User:
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+question_bank = []
 
-    def __init__(self,user_id,username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
 
-    def follow(self, user):
-        user.followers +=1
-        self.following +=1
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
-user_1 = User("001","Maksym")
-user_2 = User("oo2", "Kristina")
+while quiz.still_has_questions():
+    quiz.next_question()
 
-user_1.follow(user_2)
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
+print("You've completed the quiz")
+print(f"Your final score is {quiz.score}/{quiz.question_number}.")
 
