4c2e77f935ad7c11eed6af81dd27e969918c0a27
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-02-02 16:10:08 +0100

    Added day 18

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,26 +1,46 @@
-from turtle import Turtle, Screen
+# import colorgram
 
-tim = Turtle()
+# colors = colorgram.extract('image.jpg', 20)
+#
+# rgb_colors = []
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+import turtle as turtle_module
+import random
+
+turtle_module.colormode(255)
+
+tim = turtle_module.Turtle()
+tim.penup()
+tim.hideturtle()
+tim.setheading(220)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 101
+tim.speed("fastest")
 
 
-for shape_side_n in range(3,10):
-    draw_shape(shape_side_n)
+color_list = [(202, 172, 108), (240, 246, 244), (153, 181, 196), (192, 161, 177), (152, 186, 174), (214, 204, 114), (207, 179, 197), (163, 202, 213), (175, 188, 212), (160, 213, 187), (114, 122, 185), (214, 181, 180), (186, 157, 62), (198, 205, 49)]
+
+for dot_count in range(1, number_of_dots):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 
-
-
-
-
-
-
-
-
-
-screen = Screen()
+screen = turtle_module.Screen()
 screen.exitonclick()
