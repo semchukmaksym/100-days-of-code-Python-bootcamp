ea9d1f4791c192aefebe0e037bac80820b63755f
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-02-03 11:33:52 +0100

    Added day 19

---
+++ Day 19/turtle_sketch.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def turn_back():
+    tim.backward(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="s", fun=turn_back)
+screen.onkey(clear, "c")
+screen.exitonclick()
