ee1aea90f8d6bc46aafec88048839a928bcced05
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-02-06 12:11:04 +0100

    Finished Snake game

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,8 +1,12 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
+food = Food()
+scoreboard = Scoreboard()
 screen.setup(width=600, height=600)
 
 screen.bgcolor("black")
@@ -22,7 +26,19 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_is_on = False
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) <10:
+            game_is_on = False
+            scoreboard.game_over()
 screen.exitonclick()
--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-STARTING_POSITIONS = starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -14,11 +14,19 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
             new_segment = Turtle(shape="square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
