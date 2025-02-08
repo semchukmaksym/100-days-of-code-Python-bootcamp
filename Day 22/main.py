310a97ef49c153f9a06510ccddccc98cfb738a5e
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-02-08 13:26:01 +0100

    Completed the Pond game

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,8 +10,10 @@ screen.bgcolor("black")
 screen.title("My pong game")
 screen.tracer(0)
 
+scoreboard = Scoreboard()
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball((0,0))
 
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -21,11 +26,22 @@ screen.listen()
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
 
-
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
