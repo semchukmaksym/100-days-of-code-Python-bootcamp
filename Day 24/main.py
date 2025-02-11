cca562cbaaa1da150ccdecbf6ad45fa0eff96893
Maksym Semchuk <semchukmaksym07@gmail.com>
2025-02-11 12:46:10 +0100

    Working on day 24

---
+++ Day 24/main.py
@@ -0,0 +1,11 @@
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\n New text.")
+
+with open("../../../../Users/Damian/Desktop/my_file.txt", mode="r") as file:
+    text = file.read()
+    print(text)
