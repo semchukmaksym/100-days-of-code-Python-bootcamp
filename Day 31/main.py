9fb3fa7445746fc7fe057346ab0d3991b3c44f94
Maksym <semchukmaksym07@gmail.com>
2025-02-21 11:04:15 +0100

    Completed day 31

--- Day 31/main.py
+++ Day 31/main.py
@@ -4,14 +4,16 @@ import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+current_card = {}
+to_learn = {}
+
 try:
     data = pandas.read_csv("./data/words_to_learn.csv.csv")
 except FileNotFoundError:
     original_data = pandas.read_csv("./data/french_words.csv")
-# Convert to dictionary:
-to_learn = data.to_dict(orient="records")
-current_card = {}
-
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 def next_card():
     global current_card, flip_timer
@@ -28,12 +30,11 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+
 def is_known():
     to_learn.remove(current_card)
-    print(len(to_learn))
     data = pandas.DataFrame(to_learn)
     data.to_csv("./data/words_to_learn.csv")
-
     next_card()
 
 
@@ -52,9 +53,6 @@ card_title = canvas.create_text(400,150, text="", font=("Arial", 40, "italic"))
 card_word = canvas.create_text(400,263, text="", font=("Arial", 40, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
-
-
-
 check_image = PhotoImage(file="./images/right.png")
 known_button = Button(image=check_image, command=is_known)
 known_button.grid(row=1, column=0)
@@ -63,13 +61,6 @@ cross_image = PhotoImage(file="./images/wrong.png")
 unknown_button = Button(image=cross_image, command=next_card)
 unknown_button.grid(row=1, column=1)
 
-
-
 next_card()
-
-
-
-
-
 
 window.mainloop()
