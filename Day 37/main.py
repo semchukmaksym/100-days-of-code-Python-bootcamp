1f3ed58829fa9072767e56f84a37886e5793df64
Maksym <semchukmaksym07@gmail.com>
2025-02-28 12:36:39 +0100

    ÃLearning to work with APIs

--- Day 37/main.py
+++ Day 37/main.py
@@ -1,9 +1,11 @@
 from http.client import responses
 
 import requests
+from datetime import datetime
 
 USERNAME = "maksymbot"
 TOKEN = "ssdgsd4hsd9hsdh9ds7hsdh7"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -21,7 +23,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id" : "graph1",
+    "id" : GRAPH_ID,
     "name" : "Coding Graph",
     "unit" : "pages",
     "type" : "float",
@@ -31,7 +33,42 @@ graph_config = {
 headers = {
     "X-USER-TOKEN" : TOKEN,
 }
+# --------------------------------------POST--------------------------------------
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+#
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime(year=2025, month=2, day=27)
+
+date_strftime = today.strftime("%Y%m%d")
+
+pixel_data = {
+    "date" : date_strftime,
+    "quantity" : "10",
+}
+
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+#
+# print(response.text)
+
+pixed_edit_data = {
+    "quantity" : "5",
+    "optionalData" : "10",
+}
+
+pixel_editing_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{date_strftime}"
+
+# ------------------------------------------PUT-------------------------------------------
+
+# response = requests.put(url=pixel_editing_endpoint, json=pixed_edit_data, headers=headers)
+
+# -----------------------------------------DELETE-----------------------------------------
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{date_strftime}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
 
 print(response.text)
