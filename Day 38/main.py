16979079f9fa158a059e1573251810b11b6aeedd
Maksym <semchukmaksym07@gmail.com>
2025-03-01 16:32:40 +0100

    Working on day 38

---
+++ Day 38/main.py
@@ -0,0 +1,31 @@
+import requests
+from api import *
+
+GENDER = GENDER
+WEIGHT_KG = WEIGHT_KG
+HEIGHT_CM = HEIGHT_CM
+AGE = AGE
+
+APP_ID = API_ID
+API_KEY = API_KEY
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
